188f404986459773117c3dfebeacd08e76a58ee2
itsshady101 <rocks.ayush2011@gmail.com>
2016-07-30 17:21:46 +0530

    Models created using eloquent

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Controllers;
+
+use App\Models\User;
 use Slim\Views\Twig as View;
 
 class HomeController extends Controller
@@ -8,6 +10,10 @@ class HomeController extends Controller
 
 	public function index($request, $response) 
 	{
+		User::create([
+			'name' => 'Mia',
+			'email' => 'mia@bangbros.com'
+		])
 		return $this->view->render($response, 'home.twig');
 	}
 }
